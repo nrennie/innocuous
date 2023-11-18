0c862596ca87424fe10e5119d1117320b686bc20
nrennie <nrennie35@gmail.com>
2023-11-18 20:00:06 +0000

    add innocuous function

---
+++ R/innocuous.R
@@ -0,0 +1,10 @@
+#' Innocuous
+#'
+#' A function that returns a message
+#' @return A character string
+#' @export
+
+innocuous <- function() {
+  output <- "I am an innocuous R package."
+  return(output)
+}
